63d0841c782ff32264658c180bad180bbbbaf29f
Data-Monkey <root@tower>
2021-11-13 13:44:34 +1100

    upgrade

--- config/supporting_scripts/pihole/setGroupStatus.sh
+++ config/supporting_scripts/pihole/setGroupStatus.sh
@@ -10,9 +10,13 @@
 case "$2" in
    "enable")
        sqlite3 /etc/pihole/gravity.db "update 'group' set 'enabled'=1 where name='$1'";
+       # refresh PiHole
+       /usr/local/bin/pihole restartdns reload-lists >/dev/null       
        ;;
    "disable")
        sqlite3 /etc/pihole/gravity.db "update 'group' set 'enabled'=0 where name='$1'";
+       # refresh PiHole
+       /usr/local/bin/pihole restartdns reload-lists >/dev/null
        ;;
    "status")
        stat=`sqlite3 /etc/pihole/gravity.db " select enabled from 'group' where name = '$1';"`
@@ -23,9 +27,5 @@ case "$2" in
    echo $"Usage: $0 {GroupName enable|disable|status}"
    exit 1
 esac
-
-
-# refresh PiHole
-/usr/local/bin/pihole restartdns reload-lists >/dev/null
 
 exit 0
